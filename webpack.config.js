06e5ba34ba9f626bc7fe197e847a1421f4056027
Ernesto Jiménez <ernesto@bedu.org>
2018-12-13 20:00:54 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {test: /\.js/, use: 'babel-loader'}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'})
+  ]
+}
